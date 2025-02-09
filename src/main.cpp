64beeb57a5887706d67d417543b92ff95a1b497f
Roland <161613147+Roland525@users.noreply.github.com>
2025-02-09 11:26:21 +0000

    2

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,103 @@ class Person{
     public:
         int age;
         string name;
+
+    void displayInfo(){
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
 };
 
 class Student : public Person{
-    public:ж
+    public:
         int studentID;
 
-};ж
+    vector<Course*> enrolledCourses;
+
+    void enrollCourse(Course* course){
+
+    }
+    void displayCourses(){
+
+    }
+
+};
 
 class Professor : public Person{
     public:
-        int employeeID;ж
+        int employeeID;
+
+    vector<Course*> teachingCourses;
+
+    void assignCourse(Course* course){
+
+    }
+
+    void displayAssignedCourses(){
+
+    }
 };
 
 class Course{
     public:
         int courseID;
         string courseName;
+
+    vector<Student*> students;
+
+    void addStudent(Student* student){
+
+    }
 };
 
 class Department{
     public:
         string departmentName;
 
+    vector<Professor*> professors;
+    vector<Course*> courses;
+
+    void addProfessor(Professor* professor){
+
+    }
+    void addCourse(Course* course){
+
+    }
+    void displayProfessosAndCourses(){
+
+    }
+};
+
+int main(){
+    int user;
+    while(user != 10){
+        cout << "Choose option:" << endl << "(1) Display info, " << "(2) Change info " << "(10) Exit" << endl;
+        cin >> user;
+        if (user == 1){
+            user = 0;
+            cout << "Choose option: " << endl << "(1) Display student info" << endl << "(2) Display professor info" << endl << "(3) Display depatrment info" << endl;
+            cin >> user;
+            if (user == 1){
+                    displayInfo and displayCourses;
+
+                }
+            
+            else if(user == 2){
+                displayAssignedCourses();
+            }
+            
+            else if(user == 3){
+                displayProfessosAndCourses();
+            }
+        }
+    if(user == 2){
+        user = 0;
+        cout << "Choose option: " << endl << "(1) Add student course " << "(2) Add Professor course " << "(3) Add professor " << "(4) Add course" << endl;
+
+        if(user == 1){
+            
+        }
+    }
+    }
 };
 
